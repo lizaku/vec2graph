4090dac20264df2fc31bbb0b6c8cccb17588b46b
Elizaveta Kuzmenko <lizaku77@gmail.com>
2019-06-29 15:21:37 +0200

    pip integration

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vec2graph",
+    version="0.0.1",
+    author="Nadezda Katricheva, Alyaxey Yaskevich, Anastasiya Lisitsina, Tamara Zhordaniya, Andrey Kutuzov, Elizaveta Kuzmenko",
+    author_email="lizaku77@gmail.com",
+    description="Mini-library for producing graph visualizations from embedding models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lizaku/vec2graph",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ vec2graph/__init__.py
@@ -0,0 +1,1 @@
+name = "vec2graph"
