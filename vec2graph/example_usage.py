dd6b29e252194409d173ba04a75631b95e4f0186
akutuzov <akutuzov72@gmail.com>
2019-07-19 11:38:33 +0200

    Example script for miltiple queries (for demo)

--- vec2graph/example_usage.py
+++ vec2graph/example_usage.py
@@ -41,99 +41,100 @@ def load_embeddings(embeddings_file):
     return emb_model
 
 
-logging.basicConfig(
-    format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO
-)
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO
+    )
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-w",
-    "--word",
-    help="word to look for in the model. If omitted, random word is used",
-    default="",
-)
-parser.add_argument(
-    "-n",
-    "--nbr",
-    help="amount of neighbors to show.",
-    default=10,
-    type=int,
-)
-parser.add_argument(
-    "-e",
-    "--edge",
-    help="width of an edge (link) between nodes.",
-    default=1,
-    type=int,
-)
-parser.add_argument(
-    "-d",
-    "--depth",
-    help="recursion depth to build graphs also of neighbors of target word."
-         " Default is 0 (no neighbors)",
-    default=0,
-    type=int,
-)
-parser.add_argument(
-    "-l",
-    "--lim",
-    help="limit (threshold) of cosine similarity which should be surpassed by"
-         " neighbors to be rendered as connected. Scale is either more "
-         "than 0 and less than 1 (as real range for similarities), or"
-         " from 1 to 100 as percents. Default is 0 (all edges are preserved)",
-    default=0,
-    type=float,
-)
-parser.add_argument(
-    "-m",
-    "--model",
-    help="path to vector model file. If omitted, first model with the extension "
-         "bin.gz (as binary) or .vec.gz (as non-binary) in working directory"
-         " is loaded",
-    default="",
-)
-parser.add_argument(
-    "-o",
-    "--output",
-    help="path to the output directory where to store visualization files."
-         " If omitted, a new directory will be made in the current one, with the name"
-         " based on the timestamp",
-    default="",
-)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-w",
+        "--word",
+        help="word to look for in the model. If omitted, random word is used",
+        default="",
+    )
+    parser.add_argument(
+        "-n",
+        "--nbr",
+        help="amount of neighbors to show.",
+        default=10,
+        type=int,
+    )
+    parser.add_argument(
+        "-e",
+        "--edge",
+        help="width of an edge (link) between nodes.",
+        default=1,
+        type=int,
+    )
+    parser.add_argument(
+        "-d",
+        "--depth",
+        help="recursion depth to build graphs also of neighbors of target word."
+             " Default is 0 (no neighbors)",
+        default=0,
+        type=int,
+    )
+    parser.add_argument(
+        "-l",
+        "--lim",
+        help="limit (threshold) of cosine similarity which should be surpassed by"
+             " neighbors to be rendered as connected. Scale is either more "
+             "than 0 and less than 1 (as real range for similarities), or"
+             " from 1 to 100 as percents. Default is 0 (all edges are preserved)",
+        default=0,
+        type=float,
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        help="path to vector model file. If omitted, first model with the extension "
+             "bin.gz (as binary) or .vec.gz (as non-binary) in working directory"
+             " is loaded",
+        default="",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="path to the output directory where to store visualization files."
+             " If omitted, a new directory will be made in the current one, with the name"
+             " based on the timestamp",
+        default="",
+    )
 
-parser.add_argument(
-    "-s",
-    "--sep",
-    help="if this parameter is used, the words are split by a separator"
-         "(underscore), and only the first part is shown in visualization (E.g. "
-         "it is useful when PoS is attached to a word). By now, this "
-         "parameter accepts no value",
-    action="store_true",
-)
+    parser.add_argument(
+        "-s",
+        "--sep",
+        help="if this parameter is used, the words are split by a separator"
+             "(underscore), and only the first part is shown in visualization (E.g. "
+             "it is useful when PoS is attached to a word). By now, this "
+             "parameter accepts no value",
+        action="store_true",
+    )
 
-parser.add_argument(
-    "-js",
-    "--javascript",
-    help="path to D3.js library, can be 'web' (link to version at the D3.js "
-         "site) or 'local' (file in the directory with generated HTML, if not"
-         " present, it is downloaded from web). Default is 'web'",
-    choices=("web", "local"),
-    default="web",
-)
-args = parser.parse_args()
+    parser.add_argument(
+        "-js",
+        "--javascript",
+        help="path to D3.js library, can be 'web' (link to version at the D3.js "
+             "site) or 'local' (file in the directory with generated HTML, if not"
+             " present, it is downloaded from web). Default is 'web'",
+        choices=("web", "local"),
+        default="web",
+    )
+    args = parser.parse_args()
 
-model = load_embeddings(args.model)
+    model = load_embeddings(args.model)
 
-word = args.word
+    word = args.word
 
-visualize(
-    args.output,
-    model,
-    word,
-    depth=args.depth,
-    topn=args.nbr,
-    threshold=args.lim,
-    edge=args.edge,
-    sep=args.sep,
-    library=args.javascript,
-)
+    visualize(
+        args.output,
+        model,
+        word,
+        depth=args.depth,
+        topn=args.nbr,
+        threshold=args.lim,
+        edge=args.edge,
+        sep=args.sep,
+        library=args.javascript,
+    )
---
+++ vec2graph/usage_realtime.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+import os
+import webbrowser
+from example_usage import load_embeddings
+from vec2graph import visualize
+
+logging.basicConfig(
+    format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO
+)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "-m",
+    "--model",
+    help="path to vector model file. If omitted, first model with the extension "
+         "bin.gz (as binary) or .vec.gz (as non-binary) in working directory"
+         " is loaded",
+    default="",
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    help="path to the output directory where to store visualization files."
+         " If omitted, a new directory will be made in the current one, with the name"
+         " based on the timestamp",
+    default="",
+)
+
+parser.add_argument(
+    "-s",
+    "--sep",
+    help="if this parameter is used, the words are split by a separator"
+         "(underscore), and only the first part is shown in visualization (E.g. "
+         "it is useful when PoS is attached to a word). By now, this "
+         "parameter accepts no value",
+    action="store_true",
+)
+
+args = parser.parse_args()
+
+model = load_embeddings(args.model)
+
+while True:
+    text = input('Type your query (WORD, LIM, NR_NEIGHBORS):')
+    word, lim, nr_n = text.strip().split()
+    if '_' not in word:
+        word = word + '_NOUN'
+    visualize(
+        args.output,
+        model,
+        word,
+        topn=int(nr_n),
+        threshold=float(lim),
+        sep=args.sep
+    )
+    print('Visualization generated!')
+    filepath = os.path.join(args.output, word + ".html")
+    webbrowser.open('file://' + os.path.realpath(filepath))
